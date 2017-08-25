a6fb23d3785afd988a2fe78c4edcd341c354aa88
Adam Ramirez <aramirez001@student.butte.edu>
2017-08-25 01:15:58 +0000

    lab1.1.cpp

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,6 +1,6 @@
 Player start
-    points = 10
-    computer guess a number between 1 and 20
+    player points = 10
+    computer guesses a number between 1 and 20
 
 Player inputs guess between 1 and 20
     if guess is wrong -1 points
