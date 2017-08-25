88cb0bd721a91003e9f3ba713cb9d09449e924d4
Adam Ramirez <aramirez001@student.butte.edu>
2017-08-25 01:11:17 +0000

    lab1.1.cpp

---
+++ lab11/lab11.cpp
@@ -0,0 +1,13 @@
+Player start
+    points = 10
+    computer guess a number between 1 and 20
+
+Player inputs guess between 1 and 20
+    if guess is wrong -1 points
+        guess is high then give clue "lower"
+        guess is low then give clue "higher"
+    if guess is right game over "Player Wins"
+    if points drop to '0' "player loses, Computer wins"
+
+Repeat lines 5 - 10 until player or computer wins
+   
