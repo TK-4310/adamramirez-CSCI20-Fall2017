4b0d5c808543280381005bc65d02ccae70753852
Adam Ramirez <aramirez001@student.butte.edu>
2017-08-29 05:52:46 +0000

    lab12

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,6 +1,17 @@
-\*Created by: Adam Ramirez 
-    Created on: 8/24/2017
+//Created by: Adam Ramirez 
+//Created on: 8/24/2017
     
-*/
+
 
 #include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "  __" << endl;
+   cout << "_('')_" << endl;
+   cout << "(_ . _)" << endl;
+   cout << " /  : \ " << endl;
+   cout << "(_/  \_)" << endl;
+    
+}
