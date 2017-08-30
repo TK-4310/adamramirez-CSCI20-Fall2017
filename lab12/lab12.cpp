af0177d32a366304f2cea27629298f68c4b0d51a
Adam Ramirez <aramirez001@student.butte.edu>
2017-08-30 01:19:20 +0000

    lab12

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -9,18 +9,18 @@ using namespace std;
 int main()
 {
    cout << "   _" << endl;
-   cout << " _(")_" << endl;
+   cout << " _(\")_" << endl;
    cout << "(_ . _)" << endl;
-   cout << " / : \ " << endl;
-   cout << "(_/ \_)" << endl;
+   cout << " / : \\ " << endl;
+   cout << "(_/ \\_)" << endl;
     
 }
 
 
-/*How image should look
-   _
- _(")_
-(_ . _)
- / : \
-(_/ \_)
-/*
+//*How image should look
+//   _
+// _(")_
+//(_ . _)
+// / : \
+//(_/ \_)
+//*
