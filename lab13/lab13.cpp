5d34b6ac41c5c5f2fe8aae548e8d51400f43f4af
Adam Ramirez <aramirez001@student.butte.edu>
2017-08-30 07:00:06 +0000

    lab13

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -16,13 +16,10 @@ int main()
     int person_per2 = 33; //One net migrant every 33 sec
     int person_per3 = 8; //One birth every 8 seconds
     int person_per4 = 12; //One death every 12 seconds
-    int answer1;
     int user_input1;
+    long long int answer1;
     
-    cout << "Please enter number of years to add then hit enter. "; 
-    cin >> user_input1; //input for additional years
-    
-   person_per1 = ((( 60 / person_per1 ) * 60) * 24) * 365; //equation gives me how many people people in the US per year, (((x * 60sec)
+     person_per1 = ((( 60 / person_per1 ) * 60) * 24) * 365; //equation gives me how many people people in the US per year
    
    person_per2 = (((60 / person_per2 ) * 60 ) * 24 ) * 365; //equation gives migrant per year
    
@@ -30,17 +27,21 @@ int main()
    
    person_per4 = (((60 / person_per4 ) * 60 ) * 24 ) * 365; //equation for deaths per year
    
-   answer1 = (us_population + person_per1 + person_per2 + person_per3 - person_per4) * user_input1;
+    cout << "Please enter number of years to add then hit enter. "; 
+    cin >> user_input1; //input for additional years
+    
+   answer1 = (us_population + person_per1 + person_per2 + person_per3 - person_per4);
+   
    
    the_year = the_year + user_input1;
    
-   
+   answer1 = answer1 * user_input1; //Tried to include user_input in the first answer1 equation but my answer would keep ending up negative when testing with 8 years and above
    
     //cout << answer1; //manual test for formulas
     
     
     
-    cout << "In the year "<< the_year <<"," << " the population will be " << us_population << " in the United States.";
+    cout << "In the year "<< the_year <<"," << " the population will be " << answer1 << " in the United States.";
     
     
 }
