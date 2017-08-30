039609175e638fb72b2dc57f0725a0df9592d6bb
Adam Ramirez <aramirez001@student.butte.edu>
2017-08-30 01:58:25 +0000

    lab13

---
+++ lab13/lab13.cpp
@@ -0,0 +1,14 @@
+//Created by: Adam Ramirez 
+//Created on: 8/29/2017
+//Description: Creating a program that will print out a line of text along with a calculated set of numbers.
+//Citation and References: https://s-media-cache-ak0.pinimg.com/originals/3a/8a/2b/3a8a2b072d5b40c8b257bad632d43049.jpg
+    
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+}
