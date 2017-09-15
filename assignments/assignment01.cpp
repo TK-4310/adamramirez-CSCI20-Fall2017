d645a611b2eed33406b4fb77d22844c5475f3926
Adam Ramirez <aramirez001@student.butte.edu>
2017-09-15 01:21:28 +0000

    assignment01

--- assignments/assignment01.cpp
+++ assignments/assignment01.cpp
@@ -19,7 +19,7 @@ struct payme
 
 int main()
 {
-    double insurance_cost;
+    double insurance_cost;                  //declaring variables for any number involved in pay check
     double hourly_pay;
     double mileage_pay;
     double gross_pay;
@@ -30,37 +30,57 @@ int main()
     
     payme employee1;
     
-    cout << "Name: ";
+    cout << "Name: ";                                                   //User input for name first and last
     cin >> employee1.first_name >> employee1.last_name;
-    cout << "Hours Worked: ";
+    cout << "Hours Worked: ";                                           //pay period hours
     cin >> employee1.hours_worked;
-    cout << "Hourly Pay: ";
+    cout << "Hourly Pay: ";                                             //Rate of pay
     cin >> employee1.hourly_rate;
     
     cout << "Insurance Cost: ";
     cin >> insurance_cost;
     
-    cout << "Mileage pay, starting Odometer: ";
+    cout << "Mileage pay, starting Odometer: ";                         //input of odometer for milage pay
     cin >> starting_odometer;
     cout << "Mileage pay, ending Odometer: ";
     cin >> ending_odometer;
     cout << " " << endl;
     
     
-    hourly_pay = employee1.hourly_rate * employee1.hours_worked;
-    mileage_pay = (ending_odometer - starting_odometer) * 0.35;
-    gross_pay = hourly_pay + mileage_pay;
+    hourly_pay = employee1.hourly_rate * employee1.hours_worked;        //Calculates the users base hourly pay for the period
+    mileage_pay = (ending_odometer - starting_odometer) * 0.35;         //Calculates total milage pay
+    gross_pay = hourly_pay + mileage_pay;                               //Calculates total pay before taxes
     
     taxes = gross_pay * 0.17;
-    net_pay = gross_pay - taxes;
+    net_pay = gross_pay - taxes;                                        //Calculates users pay after taxes
     
     
-    cout << "Employee Name        " << "Hours        " << "Hourly Pay        " << "Gross Pay        " << "Net Pay" << endl;
+    cout << "Employee Name        " << "Hours        " << "Hourly Pay        " << "Gross Pay        " << "Net Pay" << endl;                                             //Output for check
     cout << " " << endl;
     cout <<employee1.first_name << " " << employee1.last_name << "         " << employee1.hours_worked << "            " << employee1.hourly_rate << "            ";
     cout  << gross_pay << "          ";
     cout << net_pay;
     
-    //up next is the outputs and make sure if else statements work 
-    
 }
+
+/* 
+Results of paycheck one: Sofia Cameron, 40, $23.57, $178.65, 3268, 4154
+    Employee Name        Hours        Hourly Pay        Gross Pay        Net Pay
+ 
+    Sofia Cameron         40            23.57            1252.9          1039.91
+
+Results of paycheck two: Richard Watson, 29, $11.50,  $344.57, 9763,  10701
+    Employee Name        Hours        Hourly Pay        Gross Pay        Net Pay
+ 
+    Richard Watson         29            11.5            661.8          549.294
+
+Results of paycheck three: Heather Hunt, 35, $60.00, $224.75, 5705, 5705
+    Employee Name        Hours        Hourly Pay        Gross Pay        Net Pay
+ 
+    Heather Hunt         35            60            2100          1743
+
+Results of paycheck four: Eddy Hall, 40, $8.25, $180.27, 12919, 13146
+     Employee Name        Hours        Hourly Pay        Gross Pay        Net Pay
+ 
+    Eddy Hall         40            8.25            409.45          339.844
+*/
