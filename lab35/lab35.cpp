e1cd1cda68ea573b29482d18fcbe96a007d4b808
Adam Ramirez <aramirez001@student.butte.edu>
2017-10-25 06:10:24 +0000

    lab35

--- lab35/lab35.cpp
+++ lab35/lab35.cpp
@@ -1,12 +1,12 @@
 //Created by: Adam Ramirez
 //Created on: 10/24/2017
 //Description: Lab 3.5 - Creating a temperature converter
-//Citation and References: 
+//Citation and References: Got some solid help from Nathan B. Pointed out some major changes I needed.
 
 #include <iostream>
 using namespace std;
 
-class TemperatureConverter{
+class TemperatureConverter{     //Beginning of class
     private:
         double kelvin_;
 
@@ -19,66 +19,62 @@ class TemperatureConverter{
         void SetTempFromFahrenheit(double fahrenheit);
         double GetTempAsFahrenheit();
         void PrintTemperatures();
-    
-  
-};
-
-TemperatureConverter::TemperatureConverter(){
+        TemperatureConverter(){     //Constructor for blank value
         kelvin_ = 0;
-        return;
-}
+        }
+ 
+        TemperatureConverter(double tempVal){       //Overloaded Constructor for values
+        kelvin_ = tempVal;
+        }
+  
+};  //End Temperature Concverter class
 
-TemperatureConverter::TemperatureConverter(double kelvinVal){
-        if(kelvinVal > 0){
+
+    void TemperatureConverter::SetTempFromKelvin(double kelvinVal){     //Gets me Kelvin into my private parameter if starting temp is kelvin
+        if (kelvinVal >= 0){                                            //Should only need this one if statement since it's if kelvin is less than 0
             kelvin_ = kelvinVal;
         }
-       
+        else{
+            cout << "Try Again, Bad Temp!";
+        }
 }
-
-
-    void TemperatureConverter::SetTempFromKelvin(double kelvinVal){
-        double celsius = 0;
-        double fahrenheit = 0;
-    
-        celsius = kelvinVal - 273.15;
-        fahrenheit = (((celsius * 9)/5) + 32);
-}
-    void TemperatureConverter::SetTempFromCelsius(double celsius){
+    void TemperatureConverter::SetTempFromCelsius(double celsius){  //Gets me kelvin from a beginning celsius value
         double kelvinVal = 0;
-        double fahrenheit = 0;
         
         kelvinVal = celsius + 273.15;
-        fahrenheit = (((celsius * 9)/5) + 32);
         
     }
     
-    void TemperatureConverter::SetTempFromFahrenheit(double fahrenheit){
+    void TemperatureConverter::SetTempFromFahrenheit(double fahrenheit){    //Gets me kelvin with a beginning fahrenheit value
         double kelvinVal = 0;
-        double celsius = 0;
+        
         
         kelvinVal = (((5 * (fahrenheit - 32 ))/ 9 ) + 273.15);
-        celsius = kelvinVal - 273.15;
+        
     }
-    double TemperatureConverter::GetTempFromKelvin(){
-        double kelvinVal = kelvin_;
-        return;
+    double TemperatureConverter::GetTempFromKelvin(){       //Gets me my private parameter
+        return kelvin_;
+        
     }
-    double TemperatureConverter::GetTempAsCelsius(){
-        celsius;
-        return;
+    double TemperatureConverter::GetTempAsCelsius(){        //Uses the private parameter to get my celsius output
+        return kelvin_ - 273.15;
+        
     }
-    double TemperatureConverter::GetTempAsFahrenheit(){
-        fahrenheit;
-        return;
+    double TemperatureConverter::GetTempAsFahrenheit(){     //Usees private parameter to get my fahrenheit output
+        return (((GetTempAsCelsius() * 9)/5) + 32);;
+        
     }
-     void TemperatureConverter::PrintTemperatures(){
-        cout << "Temperature as Kelviin: " << kelvin_;
-        cout << "Temperature as Celsius: " << celsius;
-        cout << "Temperature as Celsius: " << fahrenheit;
+
+
+     void TemperatureConverter::PrintTemperatures(){        //Prints out the values
+        cout << "Temperature in Kelvin: " << GetTempFromKelvin() << endl;
+        cout << "Temperature in Celsius: " << GetTempAsCelsius() << endl;
+        cout << "Temperature in Celsius: " << GetTempAsFahrenheit() << endl;
+        cout << " " << endl;
     } 
 
 
-
+// Pulled below to test from lab
  // This program will run the Temperature Converter Class
 
 int main ()
@@ -103,3 +99,27 @@ int main ()
     
     return 0;
 }
+/*Expected Results
+Temperature in Kelvin: 0
+Temperature in Celsius: -273.15
+Temperature in Celsius: -459.67
+ 
+Temperature in Kelvin: 274
+Temperature in Celsius: 0.85
+Temperature in Celsius: 33.53
+ 
+400.15
+Temperature in Kelvin: 400.15
+Temperature in Celsius: 127
+Temperature in Celsius: 260.6
+ 
+0.85
+Temperature in Kelvin: 274
+Temperature in Celsius: 0.85
+Temperature in Celsius: 33.53
+ 
+33.53
+Temperature in Kelvin: 274
+Temperature in Celsius: 0.85
+Temperature in Celsius: 33.53
+*/
