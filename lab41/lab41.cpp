c20f4ff0dc59096b02d401b944dbcc46f9afa37d
Adam Ramirez <aramirez001@student.butte.edu>
2017-11-01 07:49:04 +0000

    lab41

--- lab41/lab41.cpp
+++ lab41/lab41.cpp
@@ -13,30 +13,90 @@ class ArrayTools {
     public:
         
         ArrayTools(int other[], int variable){
-        //int i = 0;
-        
-        other[variable] = (1,2,3,4,5,6,7,8,9, 10);
-        other_[variable_] = other[variable];
-        // other[0] = 1;
-        // other[1] = 2;
-        // other[2] = 3;
+        int i = 0;
         
         
-        // for (i = 0; i < variable; i++){
-        //     cout << "Value: ";
-        //     cout << other[i] << endl;
-            
-            
-            
-        // }
+        for (i = 0; i < variable; i++){
+            other_[i] = other[i];
+        
+         }
         }
         
         //Find_min()
         
      void Print(){
-       other_[variable_];
+        cout << other_;
+     }
+    
+    int Find_min(int num1, int num2){
+        other_[0] = num1;
+        other_[1] = num2;
+        
+        if (other_[0] > other_[1]){
+            cout << other_[0] << endl;
+        }
+        else {
+            cout << other_[1] << endl;
+        }
+        
+        
     }
     
+    int Find_max(int num1, int num2){
+        int i;
+        
+       
+        if (other_[0] > other_[1]){
+            cout << other_[0] << endl;
+        }
+        else {
+            cout << other_[1] << endl;
+        }
+        }
+    
+    
+    int Find_sum(){
+        other_[0] = Find_min();
+        other_[1] = Find_max();
+        other_[2] = other_[0] + other_[1];
+        
+        cout << other_[2] << endl;
+     }
+    
+    int Search(int val1){
+        int i = 0;
+        
+        for (i = 0; i < val1; i++){
+            if (other_[i] == val1){
+                cout << other_[i] << endl;
+            }
+        }
+        
+    }
+    int Is_sorted(){
+        int i = 0 ;
+        
+        for (i = 0; i < 4; i++){
+            int current_small = other_[i];
+            int current_small_index = i;
+            
+            int j = i + 1;
+            for(j; j < 4; j++){
+                if( current_small > other_[j]){
+                    
+                    current_small = other_[j];
+                    current_small_index = j;
+                }
+            }
+            
+            if (current_small != i){
+                other_[current_small_index] = other_[1];
+                other_[i] = current_small;
+            }
+        }
+        
+        
+    }
 };
 
 
@@ -51,12 +111,12 @@ int main()
     }
     ArrayTools a(myArray,SIZE);
     
-    a.Print();
-    // cout<<"Min: "<<a.Find_min(0, 4)<<endl;
-    // cout<<"Max: "<<a.Find_max(5, 10)<<endl;
-    // cout<<"Sum = "<<a.Find_sum()<<endl;
-    // cout<<"Search 10"<<a.Search(10)<<endl;
-    // cout<<"Sorted? "<<a.Is_sorted()<<endl;
+    //a.Print();
+    cout<<"Min: "<<a.Find_min(0, 4)<<endl;
+    cout<<"Max: "<<a.Find_max(5, 10)<<endl;
+    cout<<"Sum = "<<a.Find_sum()<<endl;
+    cout<<"Search 10"<<a.Search(10)<<endl;
+    cout<<"Sorted? "<<a.Is_sorted()<<endl;
     
     return 0;
 }
